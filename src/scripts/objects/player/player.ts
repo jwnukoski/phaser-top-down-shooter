42af43aa448488d43a803e009fea72ca7b8371bc
jwnukoski <jwnukoski@protonmail.com>
2022-12-17 21:23:48 -0500

    fix player sprite to pointer

--- src/scripts/objects/player/player.ts
+++ src/scripts/objects/player/player.ts
@@ -9,16 +9,22 @@ export default class Player extends Phaser.GameObjects.Container {
       super(scene, x, y)
       scene.add.existing(this)
 
-      this.#aiming = new Aiming(x, y, this, scene)
-      this.add(this.#aiming)
 
-      this.#activeSprite = scene.add.sprite(0, 0, 'img-player-idle-unarmed')
+      this.#aiming = new Aiming(x, y, this, scene)
+      // this.add(this.#aiming) <- add this back in when math is fixed
+
+      this.#activeSprite = scene.add.sprite(0, 0, 'img-player-idle-unarmed').setAngle(-90)
       this.add(this.#activeSprite)
+
+      
     }
 
     preUpdate(time:number, delta:number):void {
       // Constrain velocity of player
       this.constrainVelocity(this, 250)
+
+      // TODO: Fix math in Aiming, because the X / Y values are inherited from this container when added as a child
+      this.#aiming.preUpdate(time, delta) 
     }
 
     constrainVelocity(sprite = this, maxVelocity) {
