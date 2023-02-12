4edd99e0852f7934a06eaf42a152e250b593ccb3
jwnukoski <jwnukoski@protonamil.com>
2023-02-12 17:04:32 -0500

    hud work

--- src/scripts/objects/player/hud/health.ts
+++ src/scripts/objects/player/hud/health.ts
@@ -4,16 +4,20 @@ import Player from '../player'
 
 export default class Health extends Phaser.GameObjects.Container {
     #playerRef:Player
+    #text:Phaser.GameObjects.Text
 
     constructor(scene:WorldScene, player:Player) {
         super(scene, 0, 0)
         this.#playerRef = player
         this.scene.add.existing(this)
 
+        this.#text = scene.add.text(0, 0, '0', { 
+            fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' 
+        })
     }
 
     preUpdate():void {
-        
+        this.#text.setText(`${this.#playerRef.getHealth()}%`)
     }
 }
   
