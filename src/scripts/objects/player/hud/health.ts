35ea89e43c7ef934b31598a5086bf946161410f4
jwnukoski <jwnukoski@protonamil.com>
2023-02-18 17:07:04 -0500

    hud work

--- src/scripts/objects/player/hud/health.ts
+++ src/scripts/objects/player/hud/health.ts
@@ -5,6 +5,11 @@ import Hud from './hud'
 
 export default class Health extends Phaser.GameObjects.Text {
     #playerRef:Player
+    #playerHealthLastSeen:number = 0
+    
+    #inFlash:boolean = false
+    #flashCount: number = 0
+    #flashMaxCount: number = 50
 
     constructor(player:Player, scene:WorldScene) {
         super(scene, 0, 280, '0', {
@@ -16,8 +21,51 @@ export default class Health extends Phaser.GameObjects.Text {
         this.#playerRef = player
     }
 
+    private setDefaultStyle() {
+        this.setColor('white')
+    }
+
+    private setHurtStyle() {
+        this.setColor('#ff0000')
+    }
+
+    private setHealedStyle() {
+        this.setColor('#00ff00')
+    }
+
+    private flashColor(playerHealthCurrent):void {
+        let changeColor:boolean = false
+
+        if ((this.#playerHealthLastSeen !== playerHealthCurrent) && !this.#inFlash) { 
+            this.#inFlash = true
+        }
+
+        if (this.#inFlash && (this.#flashCount < this.#flashMaxCount)) {
+            this.#flashCount++
+            changeColor = true
+        }
+
+        if (this.#inFlash && (this.#flashCount >= this.#flashMaxCount)) {
+            this.#flashCount = 0
+            changeColor = false
+            this.setDefaultStyle()
+        }
+
+        if (changeColor && (this.#playerHealthLastSeen < playerHealthCurrent)) {
+            this.setHealedStyle()
+        }
+
+        if (changeColor && (this.#playerHealthLastSeen > playerHealthCurrent)) {
+            this.setHurtStyle()
+        }
+        
+        this.#playerHealthLastSeen = playerHealthCurrent
+    }
+
     preUpdate():void {
-        this.setText(`${this.#playerRef.getHealth()}%`)
+        const playerHealthCurrent = this.#playerRef.getHealth()
+        this.setText(`${playerHealthCurrent}%`)
+        this.flashColor(playerHealthCurrent)
     }
 }
   
